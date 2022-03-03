bfa93e658fc70fd4a7cca5307c6fd01c744843af
mydatahack <thon6thon6@hotmail.com>
2022-03-03 15:40:40 +1100

    Update eslint rules

--- eslint-rules/index.js
+++ eslint-rules/index.js
@@ -2,12 +2,10 @@
 
 module.exports = {
   rules: {
-    'double-snake-on-feature-flag': require('./DoubleSnakeOnFeatureFlag.js'),
     'ban-raw-apollo-use-subscription': require('./BanRawApolloUseSubscription.js'),
     'ban-parse-float': require('./BanParseFloat.js'),
     'force-typed-api-requests': require('./ForceTypedApiRequests.js'),
     'styled-components-naming': require('./StyledComponentsNaming.js'),
     'prevent-unused-imports': require('./PreventUnusedImports.js'),
-    'missing-error-logging-in-catch': require('./MissingErrorLoggingInCatch.js'),
   },
 }
