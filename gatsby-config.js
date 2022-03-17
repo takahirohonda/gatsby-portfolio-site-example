0b5da7d8c96193da6d07b8095e6f0a535b654524
mydatahack <thon6thon6@hotmail.com>
2022-03-17 17:43:37 +1100

    Add favicons, too

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/icon.png',
+        icon: 'src/images/icon.png', // This will create favicons.
       },
     },
     'gatsby-plugin-mdx',
