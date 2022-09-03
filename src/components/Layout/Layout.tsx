dac47499654978db2b7b72b95cfdd2729dd46401
mydatahack <thon6thon6@hotmail.com>
2022-09-03 12:46:39 +1000

    feat(infra): add blog infra before making posts

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,74 @@
+import { ReactNode } from 'react'
+import { ContentContainer } from '@mdhnpm/react-mdh-blog-components'
+import { graphql, Link, useStaticQuery } from 'gatsby'
+
+import { GlobalStyles } from '../../fec'
+import { AnimatedTitle } from '../../fec/AnimatedTitle'
+import { Seo } from '../Seo'
+import { NavContainer } from './Layout.styled'
+
+interface LayoutProps {
+  children: ReactNode
+  title?: string
+  description?: string
+  image?: URL
+  path?: string
+  siteTitle: string
+}
+
+export const Layout = ({
+  title,
+  description,
+  image,
+  path,
+  siteTitle,
+  children,
+}: LayoutProps) => {
+  const data = useStaticQuery<Queries.GetSiteMetadataForSeoQuery>(graphql`
+    query GetSiteMetadataForSeo {
+      site {
+        siteMetadata {
+          description
+          image
+          siteUrl
+          title
+        }
+      }
+    }
+  `)
+  const defaults = data?.site?.siteMetadata
+  const titleHead = title || defaults?.title
+  const descriptionHead = description || defaults?.description
+  const siteUrl = new URL(
+    image || defaults?.siteUrl || '',
+    defaults?.siteUrl || ''
+  )
+  const imageHead = new URL(
+    path || defaults?.image || '',
+    defaults?.siteUrl || ''
+  )
+
+  return (
+    <>
+      <Seo
+        title={titleHead || ''}
+        description={descriptionHead || ''}
+        siteUrl={siteUrl}
+        image={imageHead}
+      />
+      <GlobalStyles />
+      <main>
+        <ContentContainer>
+          <AnimatedTitle />
+          <NavContainer>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/words">Flip Cards</Link>
+          </NavContainer>
+          <h1>{siteTitle}</h1>
+          {children}
+        </ContentContainer>
+      </main>
+    </>
+  )
+}
---
+++ src/components/PostLayout/PostLayout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+
+import { Layout } from '../Layout'
+
+interface PostLayoutProps {
+  children: ReactNode
+  pageContext: any
+}
+export const PostLayout = ({ children, pageContext }: PostLayoutProps) => {
+  const { title, description } = pageContext.frontmatter
+
+  return (
+    <Layout siteTitle={title} description={description}>
+      {children}
+    </Layout>
+  )
+}
